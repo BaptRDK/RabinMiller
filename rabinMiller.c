7420cc00414546ca0f1d69598d59b3f10943ba19
Baptiste <baptiste.deligny@protonmail.ch>
2017-09-24 16:15:32 +0200

    First version of the decompose() function

--- rabinMiller.c
+++ rabinMiller.c
@@ -1,3 +1,64 @@
 /*rabinMiller.c*/
 #include "rabinMiller.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+
+int decompose(unsigned long int* s, mpz_t d, mpz_t n)
+{
+	mpz_t ans;
+	mpz_init(ans);
+	unsigned long int modulo;
+	
+
+	//Tests if the input number n is odd
+	if(mpz_cdiv_q_ui(ans, n, 2) == 0)
+	{
+		fprintf(stderr, "Error : n must be an odd number.\n");
+		exit(1);
+	}
+	//If else tests if n is less than than 3
+	else if(mpz_cmp_ui(n, 3) < 0)
+	{
+		fprintf(stderr, "Error : n must be greater than 2\n");
+		exit(2);
+	}
+
+	mpz_t num;
+	mpz_init(num);
+	*s = 1;
+
+	//num = n-1
+	mpz_sub_ui(num, n, 1);
+
+	do
+	{
+		*s = *s + 1;
+
+		//d = num / (2**s)
+		modulo = mpz_cdiv_q_ui(d, num, 1 << *s);
+
+	//do while d is a round number
+	}while(modulo == 0);
+
+	*s = *s -1;
+	mpz_cdiv_q_ui(d, num, 1 << *s);
+	printf("s = %ld\n", *s); 
+	printf("d = ");
+	mpz_out_str(stdout, 10, d);
+	printf("\n");
+	
+	mpz_clear(num);
+	mpz_clear(ans);
+}
+
+int isWitness(mpz_t a, mpz_t n, mpz_t s , mpz_t d)
+{
+
+}
+
+int isPrime(mpz_t n)
+{
+
+}
--- rabinMiller.h
+++ rabinMiller.h
@@ -1,4 +1,31 @@
 #ifndef RABINMILLER_H
 #define RABINMILLER_H
+#include <gmp.h>
+
+/*Decomposes an integer n in s and d like (n-1)=(2^s)*d*/
+//takes:
+//	mpz_t	n
+//returns:
+//	unsigned long int*	s
+//	mpz_t			d
+//	int return code
+int decompose(unsigned long int* s, mpz_t d, mpz_t n);
+
+/*Tests if a number a is a Miller witness of n*/
+//takes:
+//	mpz_t	a
+//	mpz_t	n
+//	mpz_t	s
+//	mpz_t	d
+//returns:
+//	int	boolean answer
+int isWitness(mpz_t a, mpz_t n, mpz_t s , mpz_t d);
+
+/*Tests if a number n is prime*/
+//takes:
+//	mpz_t n
+//returns:
+//	int boolean answer
+int isPrime(mpz_t n);
 
 #endif
